6b930b2a5ffec4101e4f800dd9ff6c1977fa5a44
gyldanlourens <gyldanlourens@gmail.com>
2023-10-23 13:57:09 +0200

    Set up semantic tags & layout

---
+++ little-lemon-web-app/src/components/Nav.js
@@ -0,0 +1,16 @@
+function Nav(){
+    return (
+        <nav>
+            <ul>
+                <li><a key="home-section" href="home">Home</a></li>
+                <li><a key="about-section" href="about">About</a></li>
+                <li><a key="menu-section" href="menu">Menu</a></li>
+                <li><a key="reservations-section" href="reservations">Reservations</a></li>
+                <li><a key="order-online-section" href="order-online">Order Online</a></li>
+                <li><a key="login-section" href="login">Login</a></li>
+            </ul>
+        </nav>
+    )
+}
+
+export default Nav;
